ca599a07c7b7d7e90a309cc895314df1ad8cb381
rohapereira <rodrigo.apereira@hotmail.com>
2024-04-29 08:37:54 -0300

    commit 29-04-2024

---
+++ java-anatomia-classes/src/edu/rodrigo/segundasemana/MinhaClasse.java
@@ -0,0 +1,15 @@
+package edu.rodrigo.segundasemana;
+public class MinhaClasse {
+    public static void main(String[] args) {
+        String primeiroNome = "Rodrigo";
+        String segundoNome = "Pereira";
+
+        String nomeCompleto = nomeCompleto(primeiroNome, segundoNome);
+
+        System.out.println(nomeCompleto);
+    }
+
+    public static String nomeCompleto (String primeiroNome, String segundoNome){
+        return primeiroNome.concat(" ").concat(segundoNome);
+    }
+}
